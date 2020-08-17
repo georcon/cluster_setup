bfff51dfa5534b7cb45e90dd15cf212785219aac
George Constantine <GConstantine88@gmail.com>
2020-08-17 03:15:04 +0000

    Non-working VPN script

--- genCert.sh
+++ genCert.sh
@@ -1,3 +1,12 @@
+mkdir -p ~/pki/{cacerts,certs,private}
+chmod 700 ~/pki
+
+pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem
+pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem     --type rsa --dn "CN=ETDS Root CA" --outform pem > ~/pki/cacerts/ca-cert.pem
+pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem
+
+
+
 pki --pub --in ~/pki/private/server-key.pem --type rsa \
 | pki --issue --lifetime 1825 \
 --cacert ~/pki/cacerts/ca-cert.pem \
@@ -5,3 +14,6 @@ pki --pub --in ~/pki/private/server-key.pem --type rsa \
 --dn "CN=vpn.etds.me" --san vpn.etds.me \
 --flag serverAuth --flag ikeIntermediate --outform pem \
 >  ~/pki/certs/server-cert.pem
+
+sudo cp -r ~/pki/* /etc/ipsec.d/
+
