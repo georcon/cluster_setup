413d4ce01c11b2537a47882d385893a8ec2f0d33
George Constantine <gconstantine88@gmail.com>
2020-10-31 15:23:38 +0000

    Cleaned up network section. Cleaned up vpn section. Added netplan config for vpn.

---
+++ network/update_sysctl.sh
@@ -0,0 +1,3 @@
+diff sysctl.conf.new /etc/sysctl.conf 
+sudo mv sysctl.conf.new /etc/sysctl.conf 
+sudo sysctl -p
--- vpn/setup_strongswan.sh
+++ vpn/setup_strongswan.sh
@@ -2,10 +2,9 @@
 
 sudo apt install strongswan strongswan-pki libcharon-extra-plugins libcharon-extauth-plugins 
 sudo mv /etc/ipsec.conf{,.old}
-sudo cp ~/cluster_setup/vpn/ipsec.conf /etc/
+sudo ln -s /usr/cluster_setup/vpn/ipsec.conf /etc/
 
 sudo service strongswan-starter stop
 sudo service strongswan-starter start
 sudo service strongswan-starter status
-sudo cp -r ~/pki/* .
 
